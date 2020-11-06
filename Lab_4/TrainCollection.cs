dead93c99c38ceff36af22fff3b3b97ffb0a7b33
SuperZloyUser <superzloyuser@yandex.ru>
2020-11-06 12:59:20 +0700

    Lab_4 has been started

---
+++ Lab_4/TrainCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Lab_4
+{
+    public class TrainCollection
+    {
+        private List<Train> _trainList = new List<Train>();
+        private string Name { get; set; }
+        
+        public TrainCollection this[int pos] 
+        {
+            get
+            {
+                
+            }
+            set
+            {
+                
+            }
+        }
+
+        public bool Add(int pos, Train record)
+        {
+            
+        }
+
+        public bool Remove(int pos)
+        {
+            
+        }
+    }
+}
